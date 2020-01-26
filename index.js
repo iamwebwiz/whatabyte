76babad85d56cbe3ef62371956dbd78d30803b3c
Ezekiel <iamwebwiz@gmail.com>
2020-01-26 21:35:34 +0100

    Initialized index.js

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || '3000';
 
-app.listen(port);
+app.get('/', function(req, res) {
+  res.send('OKAY!');
+});
+
+app.listen(port, () =>
+  console.log(`Server started at: http://localhost:${port}`)
+);
