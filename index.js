df26c3e58905c5b46921089d3136ae5d6962f7c1
Ezekiel <iamwebwiz@gmail.com>
2020-01-29 05:07:17 +0100

    Add stylesheets

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || '3000';
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   res.render('index', { title: 'Home' });
